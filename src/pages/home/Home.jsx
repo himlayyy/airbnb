affa8eb0c0a0778a211d59d10923ab17ed11f59c
asus <alesandralelis@gmail.com>
2022-10-21 23:59:22 +0800

    Removed route nesting

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Header from "../../components/header/Header";
 import Destinations from "../../components/destinations/Destinations";
 import HomeFooter from "../../components/homeFooter/HomeFooter";
 import StayScroller from "../../components/stayScroller/StayScroller";
@@ -9,6 +10,8 @@ import "./home.css";
 function Home() {
   return (
     <>
+
+
       <div className="home page-padding center">
         {/* <StayScroller /> */}
        {/* <Destinations/> */}
