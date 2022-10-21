025221e9899080ce196653efdbc931cc68db2773
asus <alesandralelis@gmail.com>
2022-10-22 00:04:08 +0800

    Site logo made into a Link

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import {useNavigate, Link} from "react-router-dom";
 import "./header.css";
 
 import AccountToggle from "../accounttoggle/AccountToggle";
@@ -6,8 +7,12 @@ import SearchBar from "../searchbar/SearchBar";
 import SiteLogo from "../sitelogo/SiteLogo";
 import ExpandedSearch from "../expandedSearch/ExpandedSearch";
 
+import Home from "../../pages/home/Home";
+
 function Header() {
   const [active, setActive] = useState(null);
+
+  const navigate = useNavigate();
 
   const activeCallback = (optionName) => {
     setActive(optionName);
@@ -15,7 +20,9 @@ function Header() {
 
   return (
     <div className="header page-padding">
+      <Link to="/">
       <SiteLogo />
+      </Link>
       <div className="headerCenter">
         {active === null ? (
           <>
