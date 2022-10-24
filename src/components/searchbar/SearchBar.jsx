0cee2a2d8616309e3760f16aaf27b601079ffa9e
asus <alesandralelis@gmail.com>
2022-10-24 08:46:50 +0800

    Refactored for mobile

--- src/components/searchbar/SearchBar.jsx
+++ src/components/searchbar/SearchBar.jsx
@@ -1,34 +1,59 @@
 import React, { useState } from "react";
+
+import ExpandedSearch from "../expandedSearch/ExpandedSearch";
+
 import "./searchbar.css";
 
-import {IoSearchCircleSharp} from "react-icons/io5";
+import { IoSearchCircleSharp } from "react-icons/io5";
 
-function SearchBar({setSearchTab}) {
+function SearchBar({ setSearchTab }) {
+  const [active, setActive] = useState(null);
+  const [onClick, setOnClick] = useState(false);
+  const [openExpanded, setOpenExpanded] = useState(false)
+
   const [searchQuery, setSearchQuery] = useState("");
-  const [option, setOption] = useState('')
+  const [option, setOption] = useState("");
 
-  
   return (
-    <div className="search">
-      {/* <input
-        className="searchBox"
-        type="text"
-        placeholder="Where to?"
-        onChange={(e) => {
-          setSearchQuery(e.target.value);
-          console.log(searchQuery);
-        }}
-      />       */}
-      <button className="staysGroup searchItem" onClick={() =>setSearchTab("btn1")}>Anywhere</button>
-      <hr/>
-      <button className="experiencesGroup searchItem" onClick={() =>setSearchTab("btn2")}>Any week</button>
-      <hr/>
-      
-      <button className="onlineExperiences searchItem"  onClick={() =>setSearchTab("btn3")}>Add guests</button>
-      <button className="searchButton searchItem">
-      <IoSearchCircleSharp size={"3em"}/></button>
-    </div>
+    <>
+    {openExpanded === true ? (<ExpandedSearch activ={"btn1"} />)
+    : 
+      (
+        <div className="search">
+          <div className="searchSection">
+            <div className="searchPrompt pointer" 
+            onClick={() => setOpenExpanded(!openExpanded)}>Where to?</div>
+            <div className="searchItems">
+              <button
+                className="staysGroup searchItem"
+                onClick={() => setSearchTab("btn1")}
+              >
+                Anywhere
+              </button>
+              <hr />
+              <button
+                className="experiencesGroup searchItem"
+                onClick={() => setSearchTab("btn2")}
+              >
+                Any week
+              </button>
+              <hr />
 
+              <button
+                className="onlineExperiences searchItem"
+                onClick={() => setSearchTab("btn3")}
+              >
+                Add guests
+              </button>
+            </div>
+          </div>
+          <button className="searchButton searchItem">
+            <IoSearchCircleSharp size={"3em"} />
+          </button>
+        </div>
+      )
+  }
+    </>
   );
 }
 
