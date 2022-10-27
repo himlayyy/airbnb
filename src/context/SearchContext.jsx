47ebb02717972ea1da6e94dcae07c7364ece4cb6
asus <alesandralelis@gmail.com>
2022-10-28 03:35:35 +0800

    Added new fields guestsString & datesString to context

--- src/context/SearchContext.jsx
+++ src/context/SearchContext.jsx
@@ -3,28 +3,62 @@ import React, {useEffect, useState, createContext} from 'react';
 const SearchContext = createContext();
 
 function SearchContextProvider(props) {
-    const [search, setSearch] = useState({       
+    const [search, setSearchContext] = useState({       
     });
 
-    const updateSearchContext = (key, value) => {
-        console.log("in update search!")
-        console.log(key, value)
-        
-        setSearch((prevState) => ({...prevState, [key]:value }));
+    const updateSearchContext = (key, value) => {        
+        setSearchContext((prevState) => ({...prevState, [key]:value }));
+        console.log(search);
     }
 
     useEffect(() =>{
-        setSearch({
-        country: "",
-        dates: {},
-        guests: {}})
+        setSearchContext({
+        country: null,
+        dates: null,
+        guests: null,
+        datesString: null,
+        guestsString: null,
+        })
     }, []);  
 
     return (
         <SearchContext.Provider value={{search, updateSearchContext}}>
             {props.children}
         </SearchContext.Provider>
+       
+        
     );
 };
 
 export  {SearchContext, SearchContextProvider};
+
+
+// function SearchContextProvider(props) {
+//     const [search, setSearch] = useState({       
+//     });
+
+//     const updateSearchContext = (key, value) => {        
+//         setSearch((prevState) => ({...prevState, [key]:value }));
+//         console.log(search);
+//     }
+
+//     useEffect(() =>{
+//         setSearch({
+//         country: null,
+//         dates: null,
+//         guests: null,
+//         datesString: null,
+//         guestsString: null,
+//         })
+//     }, []);  
+
+//     return (
+//         <SearchContext.Provider value={{search, updateSearchContext}}>
+//             {props.children}
+//         </SearchContext.Provider>
+       
+        
+//     );
+// };
+
+// export  {SearchContext, SearchContextProvider};
