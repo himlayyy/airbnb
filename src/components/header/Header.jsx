e73528dd8bd52a98e704dff28d10f21951e6147c
asus <alesandralelis@gmail.com>
2022-10-31 22:58:27 +0800

    Refactored code

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
-import {useNavigate, Link} from "react-router-dom";
-import "./header.css";
+import React, { useState, useEffect, useContext } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { IoCloseCircle } from "react-icons/io5";
 
 import AccountToggle from "../accounttoggle/AccountToggle";
 import SearchBar from "../searchbar/SearchBar";
@@ -9,85 +9,102 @@ import ExpandedSearch from "../expandedSearch/ExpandedSearch";
 
 import Home from "../../pages/home/Home";
 
+import "./header.css";
+
 function Header() {
   const [active, setActive] = useState(null);
 
-  const [activeBtn, setActiveBtn] = useState(null);
+  const [activeBtn, setActiveBtn] = useState("btn1");
   const [staysClicked, setStaysClicked] = useState(false);
   const [expClicked, setExpClicked] = useState(false);
-  const [guestsClicked, setGuestsClicked] = useState(false);
-
+  const [openExpanded, setOpenExpanded] = useState(false);
+  const [closeSearch, setCloseSearch] = useState(false);
 
   const navigate = useNavigate();
 
-  const activeCallback = (optionName) => {
-    setActive(optionName);
+  const activeCallback = (tab) => {
+    setActive(!active);
+    setActiveBtn(tab)
   };
+  // const promptClicked = () =>{
+  //   setOpenExpanded(!openExpanded);
+  // };
+
+  useEffect(() => {
+console.log(`closeSearch ${closeSearch} active ${active} activeBtn ${activeBtn} `)
+  }, [closeSearch, active])
 
   return (
     <>
-    <div className="headerContainer">
-    <div className="header page-padding">
-      <Link to="/">
-      <SiteLogo className="hide1120"/>
-      </Link>
-      <div className="headerCenter">
-        {active === null ? (
-          <>
-            <SearchBar setSearchTab={activeCallback} />
-          </>
-        ) : (
-          <>
-            <div className={`expandedSearch ${active === null ? "hide" : ""}`}>
-        <div className="searchTabs">
-          <span
-            className="searchTab searchItem"
-            onClick={() => {
-              setActiveBtn("btn1");
-              setStaysClicked(!staysClicked);
-            }}
-          >
-            <button className={`${active === "btn1" ? "clickedTab" : null}`}>
-              Stays
-            </button>
-          </span>
-          <span
-            className="searchTab searchItem"
-            onClick={() => {
-              if (active !== "btn2") {
-                setExpClicked(!expClicked);
-                setActiveBtn("btn2");
-              }
-            }}
-          >
-            <button>Experiences</button>
-          </span>
-          <span
-            className="searchTab searchItem"
-            onClick={() => {
-              setActiveBtn("btn3");
-            }}
-          >
-            <button>Online Experiences</button>
-          </span>
+      <div className="headerContainer">
+        <div className="header page-padding">
+          <Link to="/">
+            <SiteLogo />
+          </Link>         
+          <div className="headerCenter">
+            {active === null ? (
+              <>
+                <SearchBar setSearchTab={activeCallback} />
+              </>
+            ) : (
+              <>
+                <div
+                  className={`expandedSearch ${active === null ? "hide" : ""}`}
+                >
+                  <div className="searchTabs">
+                    <span
+                      className="searchTab searchItem"
+                      onClick={() => {
+                        setActiveBtn("btn1");
+                        setStaysClicked(!staysClicked);
+                      }}
+                    >
+                      <button
+                        className={`${active === "btn1" ? "clickedTab" : null}`}
+                      >
+                        Stays
+                      </button>
+                    </span>
+                    <span
+                      className="searchTab searchItem"
+                      onClick={() => {
+                        if (active !== "btn2") {
+                          setExpClicked(!expClicked);
+                          setActiveBtn("btn2");
+                        }
+                      }}
+                    >
+                      <button>Experiences</button>
+                    </span>
+                    <span
+                      className="searchTab searchItem"
+                      onClick={() => {
+                        setActiveBtn("btn3");
+                      }}
+                    >
+                      <button>Online Experiences</button>
+                    </span>
+                    <button
+                      className="buttonTop"
+                      onClick={() => {
+                        setCloseSearch(!closeSearch); 
+                        setActive(null);
+                      setActiveBtn("")}}
+                    >    
+                      <IoCloseCircle size={"2.5em"} />
+                    </button>
+                  </div>
+                  
+                </div>
+              </>
+            )}           
+          </div>
+
+          <AccountToggle />
         </div>
+        {active !== null  && <ExpandedSearch active={activeBtn} /> }
       </div>
-          </>
-        )}
-      </div>
-
-      
-        <AccountToggle />
-
-    </div>
-    {active !== null ?
-
-    (<ExpandedSearch active={activeBtn}/>)
-    : 
-    ""
-    }
-    </div>
-        </>
+    </>
   );
 }
 
