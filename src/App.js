050ab6cd8f1acedb1f9ec125fa23ce454c512b0d
asus <alesandralelis@gmail.com>
2022-11-07 17:51:48 +0800

    Added document.title

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import "React", {useEffect} from "react";
 import {Route, Routes} from "react-router-dom";
 import Header from "./components/header/Header";
 // import Navbar from "./components/navbar/Navbar";
@@ -15,6 +16,10 @@ import "slick-carousel/slick/slick-theme.css";
 
 
 function App() {
+
+  useEffect(() =>{
+    document.title = "Airbnb";
+  },[]);
   
   return (
      
