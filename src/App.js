27a79355be58ec58a52a6838fe76e9ec79c3a56f
asus <alesandralelis@gmail.com>
2022-11-10 19:05:43 +0800

    Fixed import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "React", {useEffect} from "react";
+import React, {useEffect} from "react";
 import {Route, Routes} from "react-router-dom";
 import Header from "./components/header/Header";
 // import Navbar from "./components/navbar/Navbar";
