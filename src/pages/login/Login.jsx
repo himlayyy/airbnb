b1183ba7e1027096b29d1968cab8cb9ed2134358
asus <alesandralelis@gmail.com>
2022-11-11 01:12:52 +0800

    Added Login page

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./login.css";
+
+function Login() {
+  return (
+    <div className="login">
+      <div className="login-container">
+        Log-in or Sign-ip
+      </div>
+    </div>
+  )
+}
+
+export default Login;
