69fc8989c5d46cca7cd48fbcad612e84cd09cf68
himlayyy <alesandralelis@gmail.com>
2022-11-18 17:08:31 +0800

    Added quotes to URL-restricted api key

--- src/pages/searchResult/SearchResult.jsx
+++ src/pages/searchResult/SearchResult.jsx
@@ -28,7 +28,7 @@ function SearchResult() {
 
   // const mapboxAccessToken = process.env.REACT_APP_MAPBOX_KEY;
 
-  const mapboxAccessToken = pk.eyJ1IjoiZGVtYXVyaWVyIiwiYSI6ImNsYWpoOG84ZDBkNTgzb3BqOGFtZmlxd2MifQ.l3c6CXayb4EtdxMHehsCOQ
+  const mapboxAccessToken = "pk.eyJ1IjoiZGVtYXVyaWVyIiwiYSI6ImNsYWpoOG84ZDBkNTgzb3BqOGFtZmlxd2MifQ.l3c6CXayb4EtdxMHehsCOQ"
  
   useEffect(() =>{
     const handleWindowResize = () => setWidth(window.innerWidth);
