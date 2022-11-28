fc88e4762a68b0b110e641556041cb718de12d71
himlayyy <alesandralelis@gmail.com>
2022-11-28 22:11:27 +0800

    Uses browser navigator API to get current lat & long, added a new request to identify courty of current lat & long

--- src/context/Geolocation.jsx
+++ src/context/Geolocation.jsx
@@ -1,50 +1,67 @@
+import axios from "axios";
 import React, {useState, useEffect,  createContext} from "react";
-import useFetch from "../hooks/useFetch";
+import { parseItem } from "../helpers/helpers";
 
 const GeoContext = createContext();  
 
 function GeoContextProvider(props){     
-    const {data, loading, error} = useFetch("http://ip-api.com/json/?fields=status,message,continent,country,countryCode,regionName,city,zip,lat,lon,timezone,currency,query");  
+    const [data, setData] = useState({
+        country:"",
+        currency:"",
+        symbol: "",
+        language: ""
+    });
 
     const [geoContext, setGeoContext] = useState({});
+
+    const [loading, setLoading] = useState(true);
 
     const updateContext = (arr) =>{
         const temp = {}
         arr.forEach(({key, value}) =>{return temp[key]= value});
-        
-        
         (
         setGeoContext((prevState) => ({...prevState, ...temp })
         )
         );
-        console.log(geoContext);
     };
 
-    // const updateContext = ({key, value}) =>{
-    //     const temp = {...geoContext, [key] : value};
-        
-    //     (
-    //     setGeoContext({...temp}
-    //     )
-    //     );
-    //     console.log(geoContext);
-    // };
+    const  geocode = async (position) => {
 
-    useEffect(() => {
-        console.log(geoContext);
-    }, [geoContext])
+        let location = await axios
+        .get(`https://geocode.xyz/${position.coords.latitude},${position.coords.longitude}?json=1&auth=554138311287701401525x115708`)
+        .then((res) => res.data.country);
+
+        let res = await axios.get(`https://restcountries.com/v3.1/name/${location}?fields=name,currencies,languages`)
+        .then((res) => res.data[0]);
+
+        const {country, currency, language } = parseItem(res);
+        setData((prevState) => ({...prevState, country:country, currency:currency[0], symbol:currency[1], language:language}));
+        setLoading(!loading);
+    };
+
+    useEffect(() =>{
+        if (!navigator.geolocation) {
+            window.alert('Geolocation is not supported by your browser');
+        } 
+        else{
+            navigator.geolocation.getCurrentPosition((position) => 
+            {
+                geocode(position);
+            }
+            );
+        };
+    },[])
 
     useEffect(() => {
         if(!loading){
             setGeoContext({
                 ...data,
-            })
-        }
+            });
+        };
     }, [loading]);
     
     return (
         <GeoContext.Provider value={{geoContext, updateContext}} >
-            {/* {console.log(geoContext)} */}
            {props.children}
         </GeoContext.Provider>
     )
