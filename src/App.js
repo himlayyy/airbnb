a3a2d8d60fe994e37a115878f049fb01d85a962b
himlayyy <alesandralelis@gmail.com>
2022-12-29 03:11:16 +0800

    Added rooms route

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import {Route, Routes} from "react-router-dom";
 import Header from "./components/header/Header";
 import Home from "./pages/home/Home";
 import SearchResult from "./pages/searchResult/SearchResult";
-import Page2 from "./pages/page2/Page2";
+// import Page2 from "./pages/page2/Page2";
 import SignUp from "./pages/authform/signup/SignUp";
 import Login from "./pages/authform/login/Login";
+import Rooms from "./pages/rooms/Rooms";
 
 import './App.css';
 
@@ -23,9 +24,10 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/search" element={<SearchResult />}/>
-          <Route path="/page-2" element={<Page2 />} />
+          {/* <Route path="/page-2" element={<Page2 />} /> */}
           <Route path="/login" element={<Login/> } />
           <Route path="/signup" element={<SignUp />} />
+          <Route path="/rooms" element={<Rooms />} />
         </Routes>
     </div>
   );
