cae69e22cc32f6df2ce876a7450d8504f211667e
himlayyy <alesandralelis@gmail.com>
2023-01-16 10:09:13 +0800

    Added useEffect to fetch inital Destinations content based on Geolocation

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,31 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState, useContext} from "react";
 import Header from "../../components/header/Header";
 import Destinations from "../../components/destinations/Destinations";
 import HomeFooter from "../../components/homeFooter/HomeFooter";
 import StayScroller from "../../components/stayScroller/StayScroller";
 
+import { getRoomsInCountry } from "../../firebase";
+import { GeoContext } from "../../context/Geolocation";
+
+
 
 import "./home.css";
 
 function Home() {
+  const [rooms, setRooms] = useState([]);
+  const { geoContext } = useContext(GeoContext);
+
+  useEffect(() => {
+    if (geoContext.country) {
+      let roomsInCountry = getRoomsInCountry(
+        geoContext.country.toLowerCase()
+      ).then((rooms) => setRooms(rooms));
+      // setRooms(roomsInCountry);
+      console.log(rooms);
+    };
+    console.log(rooms)
+  }, [geoContext.country]);
+
   useEffect(() =>{
     document.title = "Vacation Homes & Condo Rentals";
   },[]);
@@ -18,7 +36,10 @@ function Home() {
 
       <div className="home page-padding center">
         <StayScroller />
-        <Destinations/>
+        {rooms.length !==0 && 
+          <Destinations rooms={rooms} country={geoContext.country}/>
+        }
+        
       </div>
         <HomeFooter/>
     </>
