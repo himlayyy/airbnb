f66068aae443078edbc0b8f87a02706111077a9d
himlayyy <alesandralelis@gmail.com>
2023-01-16 15:55:03 +0800

    Update DestinationItem.jsx

--- src/components/destinationItem/DestinationItem.jsx
+++ src/components/destinationItem/DestinationItem.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import { useNavigate, Link, useParams } from "react-router-dom";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Slider from "react-slick";
 
 import "./destinationItem.css";
